b996b91959e8892cb60cb1568470bd85fb2de916
Roonie <roonierue@gmail.com>
2021-12-10 06:51:16 +0200

    correct rGB values  updated

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
-const navbar = document.getElementById('navbar');
-      let scrolled = false;
+const nav = document.querySelectorAll('.navbar')
+window.addEventListener('scroll', fixNav)
 
-      window.onscroll = function () {
-        if (window.pageYOffset > 100) {
-          navbar.classList.remove('top');
-          if (!scrolled) {
-            navbar.style.transform = 'translateY(-70px)';
-          }
-          setTimeout(function () {
-            navbar.style.transform = 'translateY(0)';
-            scrolled = true;
-          }, 200);
-        } else {
-          navbar.classList.add('top');
-          scrolled = false;
-        }
-      };
-
-
-$('#navbar a, .btn').on('click', function (e) {
-    if (this.hash !== '') {
-      e.preventDefault();
-
-      const hash = this.hash;
-
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top - 100,
-        },
-        800
-      );
+function fixNav () {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
     }
-  });
+}
 
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
